c470584ffa4406e485a170bafdb105dc755dd649
david <davidmoseslieberman@gmail.com>
2020-10-04 22:16:39 +0100

    Fixed bug where formatted_csv_file was getting overwriten

--- ETL-Process/pyspark-etl.py
+++ ETL-Process/pyspark-etl.py
@@ -11,8 +11,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit
 from datetime import datetime
 import pandas as pd
-import config_var
 import os
+import sys
+sys.path.insert(1, '../Flask-Server/')
+import config_var
 
 spark = SparkSession \
 	.builder \
@@ -25,10 +27,14 @@ api_nfc_df = spark.read.csv(os.path.join("../Flask-Server/", config_var.api_csv_
 # changing all values in the timestamp column to the current timestamp
 api_nfc_df = api_nfc_df.withColumn(config_var.api_csv_file_headers[0], lit(datetime.now()))
 
+# appending newly formatted csv to the already formatted csv
+nfc_formatted_df = spark.read.csv(config_var.nfc_formatted_csv_name, header=True)
+appended_nfc_formatted_df = nfc_formatted_df.union(api_nfc_df)
+
 # exporting the pyspark dataframe to csv file
-api_nfc_df.toPandas().to_csv("NFC_formatted_data.csv", header=True)
+appended_nfc_formatted_df.toPandas().to_csv(config_var.nfc_formatted_csv_name, header=True, index=False)
 
 # rewriting the api csv file to a blank csv (with headers)
 # so that we dont reprocess the same data on the next run
-api_csv_data_file = pd.DataFrame(columns=config_var.api_csv_data_headers)
+api_csv_data_file = pd.DataFrame(columns=config_var.api_csv_file_headers)
 api_csv_data_file.to_csv(os.path.join("../Flask-Server/", config_var.api_csv_file_name), index=False)
